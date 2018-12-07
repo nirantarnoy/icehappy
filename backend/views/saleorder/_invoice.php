87e43a08ee36bd16459e17997462db43187b2fb5
tarlektarnoy <niran_tarlek@hotmail.com>
2018-12-07 21:51:52 +0700

    update

--- backend/views/saleorder/_invoice.php
+++ backend/views/saleorder/_invoice.php
@@ -66,7 +66,7 @@
                         </div>
                         <div class="col-md-12">
                             <div class="table-responsive m-t-40" style="clear: both;">
-                                <table class="table table-hover">
+                                <table class="table table-hover table-sale">
                                     <thead>
                                     <tr>
                                         <th class="text-center">#</th>
@@ -86,7 +86,7 @@
                                         <td>Milk Powder</td>
                                         <td class="text-right"><?=number_format($value->qty)?> </td>
                                         <td class="text-right"> <?=number_format($value->price,2)?> </td>
-                                        <td class="text-right"> <?=number_format($value->qty * $value->price,2)?> </td>
+                                        <td class="text-right line-total"> <?=number_format($value->qty * $value->price,2)?> </td>
                                     </tr>
                                     <?php endforeach;?>
                                     <?php endif;?>
@@ -186,4 +186,21 @@
     <!-- End footer -->
     <!-- ============================================================== -->
 </div>
+<?php
+$js=<<<JS
+var saletotal = 0;
+$(function() {
+  $("table.table-sale tbody tr").each(function() {
+     var line_total = $(this).closest("tr").find(".line-total").text();
+     console.log(line_total);
+     saletotal = parseFloat(parseFloat(saletotal) + parseFloat(line_total)).toFixed(2);
+  });
+  $(".total").text(saletotal);
+  var vat = parseFloat((saletotal *7)/100);
+  $(".vat").text(parseFloat(vat).toFixed(2));
+  $(".grandtotal").text(parseFloat(parseFloat(saletotal) + parseFloat(vat)).toFixed(2));
+});
+JS;
+$this->registerJs($js,static::POS_END);
+?>
 
